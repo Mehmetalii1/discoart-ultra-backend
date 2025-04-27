1341565f22497d456f0e8d6ac616bdbd89dfb1c3
Mehmetalii1 <mehmetalicansen995@gmail.com>
2025-04-27 18:50:32 +0300

    feat: server.js dosyası eklendi ve backend başlatıldı

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/userRoutes');
+
+// Body verisini JSON olarak alabilmek için
+app.use(express.json());
+
+// Kullanıcı API rotalarını kullan
+app.use('/api', userRoutes);
+
+// Ana Sayfa (Opsiyonel)
+app.get('/', (req, res) => {
+    res.send('DiscoArt Ultra Backend Çalışıyor!');
+});
+
+// Sunucuyu başlat
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Sunucu çalışıyor: http://localhost:${PORT}`);
+});
