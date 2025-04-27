9a18482dbf79d485e383ef60da62de3dce2de088
Mehmetalii1 <mehmetalicansen995@gmail.com>
2025-04-27 18:46:31 +0300

    feat: memoryService.js dosyasına hafıza okuma/yazma ve kullanıcı işlemleri eklendi

---
+++ services/memoryService.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+const memoryPath = path.join(__dirname, '../memory/memory.json');
+
+// Hafızayı oku
+function readMemory() {
+    const data = fs.readFileSync(memoryPath, 'utf8');
+    return JSON.parse(data);
+}
+
+// Hafızaya yaz
+function writeMemory(newData) {
+    fs.writeFileSync(memoryPath, JSON.stringify(newData, null, 2), 'utf8');
+}
+
+// Yeni kullanıcı ekle
+function addUser(userId, userData) {
+    const memory = readMemory();
+    if (memory.kullanıcılar[userId]) {
+        throw new Error('Bu kullanıcı zaten kayıtlı.');
+    }
+    memory.kullanıcılar[userId] = userData;
+    writeMemory(memory);
+}
+
+// Kullanıcı getir
+function getUser(userId) {
+    const memory = readMemory();
+    return memory.kullanıcılar[userId];
+}
+
+// Kullanıcı güncelle
+function updateUser(userId, updateData) {
+    const memory = readMemory();
+    if (!memory.kullanıcılar[userId]) {
+        throw new Error('Kullanıcı bulunamadı.');
+    }
+    memory.kullanıcılar[userId] = { ...memory.kullanıcılar[userId], ...updateData };
+    writeMemory(memory);
+}
+
+module.exports = {
+    readMemory,
+    writeMemory,
+    addUser,
+    getUser,
+    updateUser
+};
